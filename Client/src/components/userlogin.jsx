ed1ccd6d8ed5186d75f94f79eac9c64007275393
EyerusalemHawoltu <146065208+EyerusalemHawoltu@users.noreply.github.com>
2025-02-02 02:38:30 -0500

    Update userlogin.jsx

--- Client/src/components/userlogin.jsx
+++ Client/src/components/userlogin.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './CSS/UserLogin.css';
-
+// import "../App.css";
 const API = import.meta.env.VITE_BACKEND_URL;
 
 const UserLogin = ({ onLogin }) => {
